b4d4e845a93876f39dc5e37805b583190ec03360
Neha Yadav <143873299+Neha020401@users.noreply.github.com>
2026-01-14 07:16:40 +0530

    will continue later ...

--- server/src/main/java/art/example/server/security/JwtAuthenticationFilter.java
+++ server/src/main/java/art/example/server/security/JwtAuthenticationFilter.java
@@ -1,11 +1,13 @@
 package art.example.server.security;
 
+import ch.qos.logback.core.util.StringUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -17,12 +19,19 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private JwtTokenProvider tokenProvider;
 
     @Override
-    private void doFilterInternal(HttpServletRequest request,
+    protected void doFilterInternal(HttpServletRequest request,
                                   HttpServletResponse response,
                                   FilterChain filterChain
                                   ) throws ServletException, IOException {
 
-
+try{
+    String jwt = getJwtFromRequest(request);
+    if(StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)){
+        String userId = tokenProvider.getUserIdFromToken(jwt);
+    }
+} catch (RuntimeException e) {
+    throw new RuntimeException(e);
+}
     }
 
 }
