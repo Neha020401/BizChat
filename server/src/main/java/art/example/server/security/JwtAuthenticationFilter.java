2d88496e245619f35664941972d90bf4df023adf
Neha Yadav <143873299+Neha020401@users.noreply.github.com>
2026-01-15 22:08:32 +0530

    Correction in the JWT Auth Filter

--- server/src/main/java/art/example/server/security/JwtAuthenticationFilter.java
+++ server/src/main/java/art/example/server/security/JwtAuthenticationFilter.java
@@ -1,16 +1,18 @@
 package art.example.server.security;
 
-import ch.qos.logback.core.util.StringUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -26,12 +28,34 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 try{
     String jwt = getJwtFromRequest(request);
+
+    //Validate token
     if(StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)){
+        //Get  userId  from token
         String userId = tokenProvider.getUserIdFromToken(jwt);
+
+        //Create authentication object
+        UsernamePasswordAuthenticationToken authentication =
+                new UsernamePasswordAuthenticationToken(userId,null,new ArrayList<>());
+
+        //Set authentication in security context
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
-} catch (RuntimeException e) {
-    throw new RuntimeException(e);
+} catch (Exception e ) {
+    logger.error("Could not set user authentication  in security context", e);
 }
+//Continue filter chain
+filterChain.doFilter(request,response);
     }
+
+    //Extract  JWT  from Authorization header
+    private  String  getJwtFromRequest(HttpServletRequest request){
+String bearerToken = request.getHeader("Authorization");
+if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")){
+    return bearerToken.substring(7);
+}
+return null ;
+    }
+
 
 }
