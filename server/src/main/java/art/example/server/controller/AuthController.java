cae86f63cc9346a138eda7cce0ac0f1263e63fc3
Neha Yadav <143873299+Neha020401@users.noreply.github.com>
2026-01-19 21:55:10 +0530

    Added application.properties file in gitignore and authcontroller's remaining completed

--- server/src/main/java/art/example/server/controller/AuthController.java
+++ server/src/main/java/art/example/server/controller/AuthController.java
@@ -1,12 +1,15 @@
 package art.example.server.controller;
 
 import art.example.server.dto.AuthResponse;
+import art.example.server.dto.LoginRequest;
 import art.example.server.dto.SignupRequest;
 import art.example.server.service.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ResourceBundle;
 
 @RestController
 @RequestMapping("/whoAreWe/StupidCreature/")
@@ -28,6 +31,20 @@ public class AuthController
         }
     }
 
+    @PostMapping("/login")
+    public ResponseEntity login(@Valid @RequestBody LoginRequest request){
+try{
+    AuthResponse response = authService.login(request);
+    return  ResponseEntity.ok(response);
 
+} catch (RuntimeException e) {
+    return ResponseEntity.badRequest().body(e.getMessage());
+}
+    }
+
+    @GetMapping("/test")
+    public  ResponseEntity test(){
+        return  ResponseEntity.ok("Auth endpoint working");
+    }
 
 }
