2e4da20a02715ad5c35e9cd1ea87cbf995df478d
Neha Yadav <143873299+Neha020401@users.noreply.github.com>
2026-01-20 21:50:15 +0530

    Added application.properties proper path to the git ignore

--- server/src/main/java/art/example/server/controller/TestController.java
+++ server/src/main/java/art/example/server/controller/TestController.java
@@ -1,15 +1,22 @@
 package art.example.server.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/test/api ")
 public class TestController {
 
     @GetMapping("/hello")
     public String greet(){
         return "Hello there,Welcome to BiZChat";
+    }
+
+    @GetMapping("/protected")
+    public String  protectedEndPoint(Authentication authentication){
+        String userId =  (String) authentication.getPrincipal();
+        return  "Hello User! Your ID is: " + userId;
     }
 }
