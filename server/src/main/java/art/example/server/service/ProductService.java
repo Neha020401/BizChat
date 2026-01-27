e02ddab8728da89ea407bbe17564662ba528888b
Neha Yadav <143873299+Neha020401@users.noreply.github.com>
2026-01-27 21:31:46 +0530

    changes in product service page

--- server/src/main/java/art/example/server/service/ProductService.java
+++ server/src/main/java/art/example/server/service/ProductService.java
@@ -10,6 +10,7 @@ import jakarta.validation.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -89,6 +90,50 @@ return  mapToResponseWithSeller(product);
                 .map(this::mapToResponseWithSeller)
                 .collect(Collectors.toList());
     }
+
+    public List<ProductResponse> filterByPrice(Double maxPrice, Double minPrice){
+        List<Product> product = productRepository.findByPriceBetweenAndStatus(minPrice,maxPrice,"ACTIVE");
+        return  product.stream()
+                .map(this::mapToResponseWithSeller)
+                .collect(Collectors.toList());
+
+    }
+
+    public ProductResponse updateProduct(String productId, ProductRequest request,String userId){
+        Product product  = productRepository.findById(productId)
+        .orElseThrow(()-> new RuntimeException("Product no found"));
+
+        if(!product.getSellerId().equals(userId)){
+            throw new RuntimeException("You are not athourized  to update  this product");
+        }
+
+        validateCategory(request.getCategory());
+
+        product.setTitle(request.getTitle());
+        product.setDescription(request.getDescription());
+        product.setPrice(request.getPrice());
+        product.setCategory(request.getCategory());
+        product.setImages(request.getImages());
+        product.setStock(request.getStock());
+        product.setStatus(request.getStatus());
+        product.setTag(request.getTag());
+        product.setUpdatedAt(LocalDateTime.now());
+
+        // Update dimensions
+        if (request.getDimensions() != null) {
+            Product.Dimensions dims = new Product.Dimensions();
+            dims.setWidth(request.getDimensions().getWidth());
+            dims.setHeight(request.getDimensions().getHeight());
+            dims.setUnit(request.getDimensions().getUnit());
+            product.setDimensions(dims);
+        }
+
+        // Save
+        Product updatedProduct = productRepository.save(product);
+        return mapToResponseWithSeller(updatedProduct);
+
+    }
+
 
     private void validateCategory(String category) {
         List<String> validCategories  = List.of("PAINTING","SCULPTURE","DIGITAL","PHOTOGRAPHY","OTHER");
