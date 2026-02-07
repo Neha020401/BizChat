bd674f04af6040d9e4653be1408b8bd48933f098
Neha Yadav <143873299+Neha020401@users.noreply.github.com>
2026-02-07 22:41:48 +0530

    creating order (order Service)

--- server/src/main/java/art/example/server/service/OrderService.java
+++ server/src/main/java/art/example/server/service/OrderService.java
@@ -40,5 +40,70 @@ public class OrderService {
 
         User buyer = userRepository.findById(buyerId)
                 .orElseThrow(()-> new RuntimeException("Buyer not found"));
+
+        Double totalPrice = product.getPrice() * request.getQuantity();
+
+        Order order = new Order();
+        order.setBuyerId(buyerId);
+        order.setSellerId(product.getSellerId());
+        order.setProductId(product.getId());
+        order.setProductTitle(product.getTitle());
+        order.setQuantity(request.getQuantity());
+        order.setTotalPrice(totalPrice);
+
+        Order.ShippingAddress address = new Order.ShippingAddress();
+
+        address.setStreet(request.getShippingAddress().getStreet());
+        address.setCity(request.getShippingAddress().getCity());
+        address.setState(request.getShippingAddress().getState());
+        address.setZipCode(request.getShippingAddress().getZipCode());
+        address.setPhone(request.getShippingAddress().getPhone());
+        order.setShippingAddress(address);
+
+        product.setStock(product.getStock() -request.getQuantity());
+        if(product.getStock() == 0){
+            product.setStatus("SOLD");
+        }
+        productRepository.save(product);
+
+        Order savedOrder  = orderRepository.save(order);
+
+        return mapToResponse(savedOrder);
+    }
+
+
+    private OrderResponse mapToResponse(Order order){
+        User buyer = userRepository.findById(order.getBuyerId()).orElse(null);
+        User seller =  userRepository.findById(order.getSellerId()).orElse(null);
+
+        OrderResponse response = new OrderResponse();
+        response.setId(order.getId());
+        response.setBuyerId(order.getBuyerId());
+        response.setBuyerName(buyer != null ?buyer.getProfile().getName(): "Unknown");
+        response.setSellerName(seller != null ? seller.getProfile().getName() : "Unknown");
+        response.setSellerId(order.getSellerId());
+        response.setProductId(order.getProductId());
+        response.setProductTitle(order.getProductTitle());
+        response.setQuantity(order.getQuantity());
+        response.setTotalPrice(order.getTotalPrice());
+        response.setStatus(order.getStatus());
+        response.setPaymentStatus(order.getPaymentStatus());
+        response.setCreatedAt(order.getCreatedAt());
+        response.setUpdatedAt(order.getUpdatedAt());
+
+        if(order.getShippingAddress() != null){
+            OrderResponse.ShippingAddressDTO address = new OrderResponse.ShippingAddressDTO();
+            address.setState(order.getShippingAddress().getState());
+            address.setCity(order.getShippingAddress().getCity());
+            address.setCountry(order.getShippingAddress().getCountry());
+            address.setStreet(order.getShippingAddress().getStreet());
+            address.setPhone(order.getShippingAddress().getPhone());
+            address.setZipCode(order.getShippingAddress().getZipCode());
+            response.setShippingAddress(address);
+        }
+
+        return response;
+
+
     }
 }
