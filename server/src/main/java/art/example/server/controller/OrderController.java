16119a7c6e3cf5b7eb571565247a0f6913def3a4
Neha Yadav <143873299+Neha020401@users.noreply.github.com>
2026-02-11 21:40:49 +0530

    Order Controller

--- server/src/main/java/art/example/server/controller/OrderController.java
+++ server/src/main/java/art/example/server/controller/OrderController.java
@@ -46,5 +46,32 @@ public class OrderController {
         return  ResponseEntity.ok(orders);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity getOrderById(
+            @PathVariable String id,
+            Authentication authentication){
+        try{
+            String userid = (String) authentication.getPrincipal();
+            OrderResponse order = orderService.getOrderById(id,userid);
+            return ResponseEntity.ok(order);
+        }catch (RuntimeException e){
+            return  ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}/cancel")
+    public ResponseEntity cancelOrder(
+            @PathVariable String id,
+            Authentication authentication
+    ){
+     try{
+        String buyerId = (String) authentication.getPrincipal();
+        OrderResponse response = orderService.cancelOrder(id,buyerId);
+        return  ResponseEntity.ok(response);
+     } catch (RuntimeException e) {
+         throw new RuntimeException(e);
+     }
+    }
+
 
 }
