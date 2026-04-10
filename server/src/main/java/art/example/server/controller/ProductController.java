ea4fa7bb0270cc34e56aaa1b6e661ee5fee3f3e5
Neha Yadav <143873299+Neha020401@users.noreply.github.com>
2026-04-10 23:23:16 +0530

    URl issue, fixed the products but some left

--- server/src/main/java/art/example/server/controller/ProductController.java
+++ server/src/main/java/art/example/server/controller/ProductController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/BizChat/api/products")
+@RequestMapping("/BizChat/products")
 @CrossOrigin(origins = "*")
 public class ProductController {
 
